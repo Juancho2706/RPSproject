632e75ee577a2020773de3297037f182261cf263
Juan Villegas <juanmvr2706@gmail.com>
2022-11-29 16:01:02 -0300

    Condicionales y funciones terminadas, todo esta el UI en consolelog.

--- rps.js
+++ rps.js
@@ -20,18 +20,25 @@ function getComputerChoice(){
 }
 
 function playerSelection(decisionplayer){
-    decisionplayer = prompt("Coloca tu decision: (piedra, papel o tijera)");
-    decisionplayer = decisionplayer.toLowerCase();
-    if(decisionplayer == "piedra" || decisionplayer == "papel" || decisionplayer == "tijera"){
-        return decisionplayer;
-    }else{
-        alert("Tiene que ser piedra, papel o tijera, dude!...");
-    }
+    do{
+        decisionplayer = prompt("Coloca tu decision: (piedra, papel o tijera)");
+        if(decisionplayer == null){
+            alert("Juego cancelado!");
+        }else{
+            decisionplayer = decisionplayer.toLowerCase();
+            if(decisionplayer == "piedra" || decisionplayer == "papel" || decisionplayer == "tijera"){
+                return decisionplayer;
+            }else{
+                alert("Tiene que ser piedra, papel o tijera, dude!... Intenta de nuevo!");
+                decisionplayer = null;
+            }
+        }
+    }while(decisionplayer == null);
 }
 
 function rondas(ladecisionCPU, ladecisionPlayer){
     if(ladecisionCPU == ladecisionPlayer){
-        return "empate";
+        return 2; // los 2 son empate, osea no puntos
     }else{
         switch(ladecisionCPU){
             case "piedra":
@@ -40,11 +47,61 @@ function rondas(ladecisionCPU, ladecisionPlayer){
                 }else{
                     return 0;   //los 0 son gano la pc
                 }
-                break;
             case "papel":
-                
+                if(ladecisionPlayer == "piedra"){
+                    return 0;
+                }else{
+                    return 1;
+                }
+            case "tijera":
+                if(ladecisionPlayer == "papel"){
+                    return 0;
+                }else{
+                    return 1;
+                }
+            default:
+                return 2;
+        }
+    }
+}
+function juego(){
+    let resultado = 0;
+    for(let i = 0; i <= 4; i++){
+        resultado = rondas(getComputerChoice(),playerSelection());
+        console.log(resultado);
+        if(resultado == 0){
+            victoriasPC  += 1;
+            console.log('Gano la pc');
+            // alert("Gano la PC! /nPC:" + victoriasPC + " Player:" + victoriasPlayer);
+        }else if(resultado == 1){
+            victoriasPlayer += 1;
+            console.log('Gano el jugador');
+            // alert("Gano el Jugador! /nPC:" + victoriasPC + " Player:" + victoriasPlayer);
+        }else{
+            console.log('Fue empate');
+            // alert("Fue empate xD");
         }
     }
 }
 
-console.log(getComputerChoice(), playerSelection());
+let rondastotales = 0;
+let victoriasPC = 0;
+let victoriasPlayer = 0;
+juego();
+console.log("PC: "+victoriasPC+"  Player: "+victoriasPlayer);
+if(victoriasPC > victoriasPlayer){
+    victoriasPC = 0;
+    victoriasPlayer = 0;
+    rondastotales += 1;
+    console.log("GANO LA PC!, Rondas totales:"+rondastotales);
+}else if(victoriasPC < victoriasPlayer){
+    victoriasPC = 0;
+    victoriasPlayer = 0;
+    rondastotales += 1;
+    console.log("GANO EL JUGADOR!, Rondas totales:"+rondastotales);
+}else{
+    victoriasPC = 0;
+    victoriasPlayer = 0;
+    rondastotales += 1;
+    console.log("QUEDARON EMPATADOS!, Rondas totales:"+rondastotales);
+}
